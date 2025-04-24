0d03eac756172a8702c7a4e5c7edf59134ce9aa4
TebaloKokozela <johanneskokozela@gmail.com>
2025-04-25 01:21:07 +0200

    add display class functions in the utility file, unfotunately it does not work.

---
+++ utils/utilities.py
@@ -0,0 +1,19 @@
+# file contains general functions and classes that make my life easier
+# extracted from the python data science book
+
+class display(object):
+    """Display HTML representation of multiple objects"""
+    template = """<div style="float: left; padding: 10px;">
+    <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
+    </div>"""
+
+    def __init__(self, *args):
+        self.args = args
+
+    def _repr_html_(self):
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
+
+    def __repr__(self):
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
